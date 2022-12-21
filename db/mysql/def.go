56ec8361783db23c04010569c13963a288762872
zhirui.zhou <zhirui.zhou@me2zen.com>
2022-12-21 18:36:06 +0800

    add mysql json data face

--- db/mysql/def.go
+++ db/mysql/def.go
@@ -1,14 +1,21 @@
 package mysql
 
 const (
-	LazyCommandChanSize = 1024
 	ConnCheckRate = 20 //xxx seconds
 	DBPoolMin = 1
-	DBPoolMax = 64
 )
 
-//internal db server status
+//const db field
 const (
-	DBStateActive = 1
-	DBStateDown = 0
+	TableFieldOfMax = "max"
+	TableFieldOfTotal = "total"
+	TableFieldOfData = "data" //db data field
+)
+
+//where kind
+const (
+	WhereKindOfGen = iota
+	WhereKindOfIn	  //for in('x','y')
+	WhereKindOfInSet  //for FIND_IN_SET(val, `x`, 'y')
+	WhereKindOfAssigned //for assigned condition, like '>', '<', '!=', etc.
 )
