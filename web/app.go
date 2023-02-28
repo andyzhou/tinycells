d900ba6654ca8eb8d0507a7fdfee72ab2db2fbcb
Andy Zhou <zhirui1977@163.com>
2023-02-28 13:47:31 +0800

    add web face

---
+++ web/app.go
@@ -0,0 +1,134 @@
+package web
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"os"
+	"sync"
+)
+
+/*
+ * face for gin app
+ */
+
+//request path
+const (
+	AnyPath = "any"
+)
+
+//sub web app interface
+type IWebSubApp interface {
+	Entry(c *gin.Context)
+}
+
+//face info
+type App struct {
+	port int //web port
+	server *gin.Engine //gin server
+	tplPath string //tpl root path
+	//runner *iris.Runner //iris runner
+	wg sync.WaitGroup
+}
+
+//construct
+func NewApp(g ...*gin.Engine) *App {
+	var (
+		s *gin.Engine
+	)
+	//check
+	if g != nil && len(g) > 0 {
+		s = g[0]
+	}else{
+		s = gin.Default()
+	}
+	//self init
+	this := &App{
+		server: s,
+	}
+	return this
+}
+
+//stop app
+func (f *App) Stop() {
+	f.wg.Done()
+}
+
+//start app
+func (f *App) Start(port int) bool {
+	if port <= 0 {
+		return false
+	}
+
+	//set port and address
+	f.port = port
+	addr := fmt.Sprintf(":%v", port)
+
+	//start app
+	f.wg.Add(1)
+	f.server.Run(addr)
+	f.wg.Wait()
+	return true
+}
+
+//register root app entry
+//url like: /xxx or /xxx/{ParaName:string|integer}
+func (f *App) RegisterSubApp(
+			reqUrlPara string,
+			face IWebSubApp,
+		) bool {
+	//check
+	if reqUrlPara == "" || face == nil {
+		return false
+	}
+
+	//root request route
+	requestAnyPath := fmt.Sprintf("/%v/*%v", reqUrlPara, AnyPath)
+
+	//set get、post request
+	f.server.Any(requestAnyPath, face.Entry)
+	return true
+}
+
+//get gin engine
+func (f *App) GetGin() *gin.Engine {
+	return f.server
+}
+
+//set static file path
+func (f *App) SetStaticPath(url, path string) bool {
+	if url == "" || path == "" {
+		return false
+	}
+	f.server.Static(url, path)
+	return true
+}
+
+//set tpl root path
+func (f *App) SetTplPath(path string) bool {
+	if path == "" {
+		return false
+	}
+	//set tpl path
+	f.tplPath = path
+
+	//check path
+	info, err := os.Stat(path)
+	if err != nil || info == nil {
+		return false
+	}
+	if !info.IsDir() {
+		return false
+	}
+
+	//init templates
+	f.server.LoadHTMLGlob(fmt.Sprintf("%v/*", f.tplPath))
+	return true
+}
+
+//set gin reference
+func (f *App) SetGin(gin *gin.Engine) {
+	if gin == nil {
+		return
+	}
+	f.server = gin
+}
---
+++ web/cookie.go
@@ -0,0 +1,143 @@
+package web
+
+import (
+	"errors"
+	"github.com/andyzhou/tinycells/crypt"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/securecookie"
+	"sync"
+)
+
+/*
+ * face for cookie
+ */
+
+//internal macro defines
+const (
+	CookieHashKey = "04DgUStWPtd0aJmiR+L2z5xwbpPr/hmH" //"the-big-and-secret-fash-key-here"//
+	CookieBlockKey = "5oR820YTUcjpBFAYlgbte8jEL9o2oHI2" //"lot-secret-of-characters-big-too"//
+	CookieExpireSeconds = 86400 //xxx seconds
+)
+
+//global variable for single instance
+var (
+	_cookie *Cookie
+	_cookieOnce sync.Once
+)
+
+//face info
+type Cookie struct {
+	secureCookie *securecookie.SecureCookie `secure cookie instance`
+	jwt *crypt.Jwt
+	expireTime int
+}
+
+//get single instance
+func GetCookie() *Cookie {
+	_cookieOnce.Do(func() {
+		_cookie = NewCookie()
+	})
+	return _cookie
+}
+
+//construct
+func NewCookie() *Cookie {
+	//self init
+	this := &Cookie{
+		expireTime:CookieExpireSeconds,
+		jwt: crypt.NewJwt(),
+	}
+
+	//inter init
+	this.interInit()
+	return this
+}
+
+///////
+//api
+//////
+
+//set expire time
+func (f *Cookie) SetExpireTime(seconds int) {
+	f.expireTime = seconds
+}
+
+//delete cookie
+func (f *Cookie) DelCookie(name,
+	domain string,
+	c *gin.Context,
+) error {
+	//check
+	if name == "" || c == nil {
+		return errors.New("invalid parameter")
+	}
+	//destroy cookie
+	c.SetCookie(name, "", -1, "/", domain, false, true)
+	return nil
+}
+
+//get cookie
+func (f *Cookie) GetCookie(
+			key string,
+			c *gin.Context,
+		) (map[string]interface{}, error) {
+	//check
+	if key == "" || c == nil {
+		return nil, errors.New("invalid parameter")
+	}
+	//get original value
+	orgVal, err := c.Cookie(key)
+	if err != nil {
+		return nil, err
+	}
+	//try decode pass jwt
+	jwt, err := f.jwt.Decode(orgVal)
+	if err != nil {
+		return nil, err
+	}
+	return jwt, nil
+}
+
+//set cookie
+func (f *Cookie) SetCookie(
+			key string,
+			val map[string]interface{},
+			expireSeconds int,
+			domain string,
+			c *gin.Context,
+		) error {
+	//check
+	if key == "" || val == nil || c == nil {
+		return errors.New("invalid parameter")
+	}
+	//try encode pass jwt
+	encStr, err := f.jwt.Encode(val)
+	if err != nil {
+		return err
+	}
+	//set into cookie
+	c.SetCookie(key, encStr, expireSeconds, "/", domain, false, true)
+	return nil
+}
+
+//set jwt
+func (f *Cookie) SetJwt(jwt *crypt.Jwt) bool {
+	if jwt == nil {
+		return false
+	}
+	f.jwt = jwt
+	return true
+}
+
+//////////////
+//private func
+//////////////
+
+func (f *Cookie) interInit() {
+	//init security key
+	hashKey := []byte(CookieHashKey)
+	blockKey := []byte(CookieBlockKey)
+
+	//init cookie
+	f.secureCookie = securecookie.New(hashKey, blockKey)
+}
