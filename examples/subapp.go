e5f4b06ef17c106c6bfeb697cc8972ba0d84f3f9
Andy Zhou <zhirui1977@163.com>
2023-02-28 13:48:35 +0800

    add examples

---
+++ examples/subapp.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//inter macro define
+const (
+	TplFile = "example.html"
+)
+
+type View struct {
+	Title string `json:"title"`
+	Nick string `json:"nick"`
+}
+
+//face
+type SubApp struct {
+
+}
+
+//construct
+func NewSubApp() *SubApp {
+	this := &SubApp{}
+	return this
+}
+
+//main entry
+func (f *SubApp) Entry(c *gin.Context) {
+	var (
+		view View
+	)
+
+	//get request form
+
+	//set view
+	view.Title = "Home"
+	view.Nick = "You"
+
+	//output page
+	c.HTML(http.StatusOK, TplFile, view)
+}
